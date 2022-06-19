536b2e027c899b83c63cb74e1582ad2ff853adc8
fan yan <1844609673@qq.com>
2022-06-19 17:13:09 +0800

    添加项目文件。

---
+++ FTPManager/UserControls/Menubutton.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTPManager.UserControls
+{
+    /// <summary>
+    /// Menubutton.xaml 的交互逻辑
+    /// </summary>
+    public partial class Menubutton : UserControl
+    {
+        public Menubutton()
+        {
+            InitializeComponent();
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon",typeof(PackIconMaterialKind),typeof(Menubutton));
+        public PackIconMaterialKind Icon
+        {
+            get => (PackIconMaterialKind)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+        public static readonly DependencyProperty IsActiveProperty =
+           DependencyProperty.Register("IsActive", typeof(bool), typeof(Menubutton));
+        public bool IsActive
+        {
+            get => (bool)GetValue(IsActiveProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
